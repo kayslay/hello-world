60b481f23e781ff67351e1e585a93ebb496b6372
badewa kayode <badewakayode1@gmail.com>
2017-06-19 01:21:52 +0100

    again

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 var expect = require("chai").expect
 
 describe('testing',function(){
-    it('should return true', ()=>{
+    it('should return true', function(){
 expect(true).to.be.equal(true)
     })
 })
